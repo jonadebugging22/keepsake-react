dbef8c6e65b2ebd4af2bbbdf13503735739c8794
Jonalyn <villagenjonalyne@gmail.com>
2026-07-26 18:09:58 +0800

    updated logo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon-32.png', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Keepsake',
+        short_name: 'Keepsake',
+        description: 'a cozy little place for your pictures, videos & songs',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#EFE6D6',
+        theme_color: '#A9793E',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
